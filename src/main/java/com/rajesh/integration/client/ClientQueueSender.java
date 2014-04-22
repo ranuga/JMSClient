35c798008240d06f1f3aa648207be51ff0948e23
Rajesh Anuga <ranuga@gmail.com>
2014-04-22 08:23:22 -0400

    first commit

---
+++ src/main/java/com/rajesh/integration/client/ClientQueueSender.java
@@ -0,0 +1,36 @@
+package com.rajesh.integration.client;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientQueueSender {
+
+    @Autowired
+    @Qualifier("jmsProducerTemplate")
+    private JmsTemplate jmsTemplate;
+
+    public void setJmsTemplate(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    private String jmsQueue
+
+    public void sendMesage() {
+        MessageCreator messageCreator=new MessageCreator() {
+            public Message createMessage(Session session) throws
+                    JMSException {
+                return session.createTextMessage("I am sending Invoice message");
+            }
+        };
+
+        jmsTemplate.send("jms/testQueue", messageCreator);
+    }
+}
